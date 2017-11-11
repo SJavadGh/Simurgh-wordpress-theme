c3755f625f11a8b6a7efe8a0a5cda38face59dbc
AliTaee <ali.taee.72@gmail.com>
2017-11-11 10:33:34 +0330

    Add last.fm icon to menu-social and bug fix in menu

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'simurgh' ); ?></button>
 			<?php
 				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
+					'theme_location' => 'primary',
 					'menu_id'        => 'primary-menu',
 				) );
 			?>
