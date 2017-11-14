3d228ca43267fe8341d0d4a622ba236ac40e33ad
AliTaee <ali.taee.72@gmail.com>
2017-11-14 18:44:31 +0330

    Design single post

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -26,17 +26,17 @@ if ( ! function_exists( 'simurgh_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'simurgh' ),
+			esc_html_x( ' %s', 'post date', 'simurgh' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'simurgh' ),
+			esc_html_x( 'Written by %s', 'post author', 'simurgh' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
-		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+		echo '</span><span class="byline"> ' . $byline . '</span>' . '<span class="posted-on">' . $posted_on; // WPCS: XSS OK.
 
 	}
 endif;
@@ -46,22 +46,6 @@ if ( ! function_exists( 'simurgh_entry_footer' ) ) :
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
 	function simurgh_entry_footer() {
-		// Hide category and tag text for pages.
-		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'simurgh' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'simurgh' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-			}
-
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'simurgh' ) );
-			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'simurgh' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-			}
-		}
 
 		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
+
+	<?php
+		
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'simurgh' ) );
+		if ( $categories_list ) {
+			/* translators: 1: list of categories. */
+			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'simurgh' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
@@ -21,6 +30,13 @@
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php simurgh_posted_on(); ?>
+			<?php 
+    			if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) { 
+        			echo '<span class="comments-link">';
+        			comments_popup_link( __( 'Leave a comment', 'simurgh' ), __( '1 Comment', 'simurgh' ), __( '% Comments', 'simurgh' ) );
+        			echo '</span>';
+    			}
+			?>
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
@@ -49,6 +65,10 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		
+		<?php
+    		echo get_the_tag_list( '<ul><li><i class="fa fa-tag"></i>', '</li><li><i class="fa fa-tag"></i>', '</li></ul>' );
+		?>
 		<?php simurgh_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
