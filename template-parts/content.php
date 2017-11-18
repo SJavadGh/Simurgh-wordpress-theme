15cf56d2dc3add3eefc3e83b5e8a70db744036ce
AliTaee <ali.taee.72@gmail.com>
2017-11-18 11:14:33 +0330

    image thumbnail - single post

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,6 +10,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	
+	<?php 
+    	if (has_post_thumbnail()) {
+	        echo '<div class="single-post-thumbnail clear">';
+        	echo the_post_thumbnail('large-thumb');
+        	echo '</div>';
+    	}
+	?>
+                
+
 	<header class="entry-header">
 
 	<?php
