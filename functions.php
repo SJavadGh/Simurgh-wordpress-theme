1f9a742f0f40a61d6caca6469cc669a270deab42
AliTaee <ali.taee.72@gmail.com>
2017-11-19 21:09:31 +0330

    Change Post and index Thumbnails size

--- functions.php
+++ functions.php
@@ -41,8 +41,8 @@ if ( ! function_exists( 'simurgh_setup' ) ) :
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
 		add_theme_support( 'post-thumbnails' );
-		add_image_size('large-thumb' , 1060 , 650 , true);
-		add_image_size('index-thumb' , 1060 , 300 , true);
+		add_image_size('large-thumb' , 1040 , 650 , true);
+		add_image_size('index-thumb' , 1040 , 400 , true);
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
