48128c63a20cb167870c97adf979451da5dd70f8
AliTaee <ali.taee.72@gmail.com>
2017-11-22 10:13:14 +0330

    Paging navigation

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -6,6 +6,55 @@
  *
  * @package Simurgh
  */
+if ( ! function_exists ( 'simurgh_paging_nav' ) ) :
+
+	function simurgh_paging_nav() {
+		// Don't print empty markup if there's only one page.
+		if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+			return;
+		}
+	
+		$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+		$pagenum_link = html_entity_decode( get_pagenum_link() );
+		$query_args   = array();
+		$url_parts    = explode( '?', $pagenum_link );
+	
+		if ( isset( $url_parts[1] ) ) {
+			wp_parse_str( $url_parts[1], $query_args );
+		}
+	
+		$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+		$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+	
+		$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+		$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+	
+		// Set up paginated links.
+		$links = paginate_links( array(
+			'base'     => $pagenum_link,
+			'format'   => $format,
+			'total'    => $GLOBALS['wp_query']->max_num_pages,
+			'current'  => $paged,
+			'mid_size' => 2,
+			'add_args' => array_map( 'urlencode', $query_args ),
+			'prev_text' => __( '← Previous', 'simurgh' ),
+			'next_text' => __( 'Next →', 'simurgh' ),
+			'type'      => 'list',
+		) );
+	
+		if ( $links ) :
+	
+		?>
+		<nav class="navigation paging-navigation" role="navigation">
+			<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'simurgh' ); ?></h1>
+				<?php echo $links; ?>
+		</nav><!-- .navigation -->
+		<?php
+		endif;
+	}
+
+endif;
+                
 
 if ( ! function_exists( 'simurgh_posted_on' ) ) :
 	/**
